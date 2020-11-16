4883eed1d14c502d24cdeb3c06b11ecfb5c4776e
Tony Watkins <tony@llifion.net>
2020-11-16 19:36:06 +0000

    Demonstrate accessing a javascript function within jsx

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function getButtonText() {
+    return 'Click on me!';
+}
+
 // create a react component
 
 const App = () => {
@@ -10,7 +14,7 @@ const App = () => {
         <div>
             <label className="label" for="name">Enter name:</label>
             <input id="name" type="text" />
-            <button style={{ backgroundColor: 'blue', color: 'white' }}>{buttonText}</button>
+            <button style={{ backgroundColor: 'blue', color: 'white' }}>{getButtonText()}</button>
         </div >
     );
 };

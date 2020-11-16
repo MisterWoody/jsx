aa4783e4f39642f286bc4d955d462816c5716a40
Tony Watkins <tony@llifion.net>
2020-11-16 19:05:05 +0000

    Set up app component and render a simple label, input field and button; Note jsx has some differences to html such as inline styles - note for attribures with dashes - remove the dash and capitalize the second word; also note replace quotes with two pairs of curly braces. The first indicates a javascript variable, the second indicates a javascript object

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
+// import the React and ReactDom libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+// create a react component
+
+const App = () => {
+    return (
+        <div>
+            <label class="label" for="name">Enter name:</label>
+            <input id="name" type="text" />
+            <button style={{ backgroundColor: 'blue', color: 'white' }}>Submit</button>
+        </div >
+    );
+};
+
+
+// take the react component and show it on the screen
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />,
+    document.querySelector('#root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();

d4cde817cf6e217d2e2c761191f2322eb159564f
Tony Watkins <tony@llifion.net>
2020-11-19 21:22:41 +0000

    It is possible to refer to javascript objects in the context where plain text is not expected though

--- src/index.js
+++ src/index.js
@@ -6,11 +6,12 @@ import ReactDOM from 'react-dom';
 
 const App = () => {
     const buttonText = { text: 'Click me' };
+    const style = { backgroundColor: 'blue', color: 'white' };
     return (
         <div>
             <label className="label" for="name">Enter name:</label>
             <input id="name" type="text" />
-            <button style={{ backgroundColor: 'blue', color: 'white' }}>{buttonText.text}</button>
+            <button style={style}>{buttonText.text}</button>
         </div >
     );
 };
